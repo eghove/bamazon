595ec2a1b9e7ea1de7d9b4d3f16b9286f17bb6b5
That_Guy <eghove@gmail.com>
2018-12-09 12:30:52 -0600

    getting database connection established, basic functions

--- bamazon.js
+++ bamazon.js
@@ -13,10 +13,30 @@ const inquirer = require('inquirer');
 const mysql = require("mysql");
 
 //==GLOBAL VARIABLES=========================================
-
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: keys.mySQL.secret,
+    database: "bamazon_DB"
+});
 
 
 //==FUNCTIONS=========================================
+
+//open connection function
+function openConnection() {
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId);
+    });
+}
+
+//close connection function
+function closeConnection() {
+    connection.end();
+    console.log("Connection closed.");
+}
 
 
 
@@ -25,3 +45,5 @@ const mysql = require("mysql");
 
 
 //==MAIN PROCESSES=========================================
+openConnection();
+closeConnection();
