20b94fae9fa41c8aa89024228e627511579a0f76
That_Guy <eghove@gmail.com>
2018-12-09 11:24:52 -0600

    setting up the bamazon.js file

---
+++ bamazon.js
@@ -0,0 +1,27 @@
+//==REQUIREMENTS & MODULES=========================================
+
+//require the dotenv module
+require("dotenv").config();
+
+//loads the exported pieces from keys.js
+const keys = require("./keys.js");
+
+//loads the inquirer module
+const inquirer = require('inquirer');
+
+//loads the mySQL module
+const mysql = require("mysql");
+
+//==GLOBAL VARIABLES=========================================
+
+
+
+//==FUNCTIONS=========================================
+
+
+
+
+
+
+
+//==MAIN PROCESSES=========================================
